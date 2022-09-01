d6c8a6346a3897a9897f4c3baed7b00cffdd5d49
ProdigyX6217 <dominic.holder.15@cityas.org>
2022-09-01 00:04:46 -0400

    Got rid of Storyboard & Setup Root View Controller

---
+++ programmaticCardWorkout/CardSelectionVC.swift
@@ -0,0 +1,18 @@
+//
+//  CardSelectionVC.swift
+//  programmaticCardWorkout
+//
+//  Created by Student Laptop_7/19_1 on 8/31/22.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+    
+}
