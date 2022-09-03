dd9048aa541b6fa11de57d66fb6a3ca9d6f5539a
ProdigyX6217 <dominic.holder.15@cityas.org>
2022-09-02 22:52:45 -0400

    Setup Card Selection Screen

--- programmaticCardWorkout/CardSelectionVC.swift
+++ programmaticCardWorkout/CardSelectionVC.swift
@@ -9,10 +9,73 @@ import UIKit
 
 class CardSelectionVC: UIViewController {
 
+    let cardImageView = UIImageView()
+    let pauseButton = CWButton(backgroundColor: .systemRed, title: "PAUSE")
+    let resetButton = CWButton(backgroundColor: .systemGreen, title: "Reset")
+    let rulesButton = CWButton(backgroundColor: .systemBlue, title: "Rules")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .red
+        view.backgroundColor = .systemBackground
+        configureUI()
     }
     
+    func configureUI() {
+        configureCardImageView()
+        configurePauseButton()
+        configureResetButton()
+        configureRulesButton()
+    }
+    
+    
+    func configureCardImageView() {
+        view.addSubview(cardImageView)
+        cardImageView.translatesAutoresizingMaskIntoConstraints = false
+        cardImageView.image = UIImage(named: "AS")
+        
+        NSLayoutConstraint.activate([
+            cardImageView.widthAnchor.constraint(equalToConstant: 250),
+            cardImageView.heightAnchor.constraint(equalToConstant: 350),
+            cardImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            cardImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -75)
+        ])
+    }
+    
+    
+    func configurePauseButton() {
+        view.addSubview(pauseButton)
+        
+        NSLayoutConstraint.activate([
+            pauseButton.widthAnchor.constraint(equalToConstant: 260),
+            pauseButton.heightAnchor.constraint(equalToConstant: 50),
+            pauseButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            pauseButton.topAnchor.constraint(equalTo: cardImageView.bottomAnchor, constant: 30)
+        ])
+    }
+    
+    
+    func configureResetButton() {
+        view.addSubview(resetButton)
+        
+        NSLayoutConstraint.activate([
+            resetButton.widthAnchor.constraint(equalToConstant: 115),
+            resetButton.heightAnchor.constraint(equalToConstant: 50),
+            resetButton.leadingAnchor.constraint(equalTo: pauseButton.leadingAnchor),
+            resetButton.topAnchor.constraint(equalTo: pauseButton.bottomAnchor, constant: 20)
+        ])
+    }
+    
+    
+    func configureRulesButton() {
+        view.addSubview(rulesButton)
+        
+        NSLayoutConstraint.activate([
+            rulesButton.widthAnchor.constraint(equalToConstant: 115),
+            rulesButton.heightAnchor.constraint(equalToConstant: 50),
+            rulesButton.trailingAnchor.constraint(equalTo: pauseButton.trailingAnchor),
+            rulesButton.topAnchor.constraint(equalTo: pauseButton.bottomAnchor, constant: 20)
+        ])
+    }
+    
+
 }
